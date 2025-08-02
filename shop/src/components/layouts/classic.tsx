b0939910f579c1a2dae6a10d3bd7239eaa61b404
Avenir Labs <a@avenirlabs.com>
2025-08-02 14:02:29 +0530

    Update classic.tsx

--- shop/src/components/layouts/classic.tsx
+++ shop/src/components/layouts/classic.tsx
@@ -2,6 +2,7 @@ import Banner from '@/components/banners/banner';
 import PromotionSliders from '@/components/promotions/promotions';
 import Categories from '@/components/categories/categories';
 const ScrollElement: React.FC<any> = require('react-scroll').Element;
+
 import FilterBar from './filter-bar';
 import ProductGridHome from '@/components/products/grids/home';
 import type { HomePageProps } from '@/types';
@@ -12,10 +13,7 @@ export default function ClassicLayout({ variables }: HomePageProps) {
       <Banner layout="classic" variables={variables.types} />
       <PromotionSliders variables={variables.types} />
       <FilterBar variables={variables.categories} />
-      <ScrollElement
-        name="grid"
-        className="flex border-t border-solid border-border-200 border-opacity-70"
-      >
+      <ScrollElement name="grid" className="flex border-t border-solid border-border-200 border-opacity-70">
         <Categories layout="classic" variables={variables.categories} />
         <ProductGridHome
           className="px-4 pt-3.5 pb-16 lg:p-6 xl:p-8"
